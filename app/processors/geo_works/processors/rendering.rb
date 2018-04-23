37739af7fc565b421cb261e4bfb8749b6dd85769
Jennifer Lindner <jenlindner@gmail.com>
2018-04-23 07:45:35 -0400

    fix string sql syntax error in shapelayer rendering

--- app/processors/geo_works/processors/rendering.rb
+++ app/processors/geo_works/processors/rendering.rb
@@ -41,7 +41,7 @@ module GeoWorks
           def add_shapefile_layer(in_path, map)
             Dir.glob("#{in_path}/*.shp").each do |shp|
               map.layer shp do |l|
-                l.query "select * from '#{File.basename shp, '.shp'}'" do |q|
+                l.query %(select * from "#{File.basename shp, '.shp'}") do |q|
                   q.styles SimplerTiles.config.to_h
                 end
               end
